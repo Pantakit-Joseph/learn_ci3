6fa8b71551dcafc7dd0cd676befee608534c473a
pantakit <mr.pantakit@gmail.com>
2021-08-22 03:30:38 +0700

    v0

--- application/views/auth/login_form_o.php
+++ application/views/auth/login_form_o.php
@@ -41,7 +41,7 @@ $captcha = array(
 	<tr>
 		<td><?php echo form_label('Password', $password['id']); ?></td>
 		<td><?php echo form_password($password); ?></td>
-		<td style="color: red;"><?php echo form_error($password['name']); ?><?php echo isset($errors[$password['name']])?$errors[$password['name']]:''; ?></td>
+		<td style="color: red;"><?php echo  ($password['name']); ?><?php echo isset($errors[$password['name']])?$errors[$password['name']]:''; ?></td>
 	</tr>
 
 	<?php if ($show_captcha) {
